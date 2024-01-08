767284096e817d4eb08b391af860ae943a6f0754
ruilonbl <ruilonbl@gmail.com>
2024-01-07 21:48:48 -0300

    migrações

--- AspNet/AspNet/Data/AspNetContext.cs
+++ AspNet/AspNet/Data/AspNetContext.cs
@@ -14,6 +14,8 @@ namespace AspNet.Data
         {
         }
 
-        public DbSet<AspNet.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller{ get; set; } = default!;
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
     }
 }
---
+++ AspNet/AspNet/Models/SalesRecord.cs
@@ -0,0 +1,25 @@
+﻿using AspNet.Models.Enums;
+
+namespace AspNet.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amout { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
+        public SalesRecord()
+        {
+        }
+        public SalesRecord(int id, DateTime date, double amout, SalesStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amout = amout;
+            Status = status;
+            Seller = seller;
+        }
+        
+    }
+}
